0a1787b43c15423ffb1f56905e1b38e8074fc7c1
uche-inyama <depoulet@yahoo.com>
2022-04-29 23:04:17 +0000

    add search form

--- src/containers/SearchForm.js
+++ src/containers/SearchForm.js
@@ -2,35 +2,27 @@ import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { fetchCountry } from '../redux/searchFormSlice';
 
-const FilterForm = () => {
-  const [region, setRegion] = useState('')
+const SearchForm = () => {
+  const [country, setCountry] = useState('')
   const dispatch = useDispatch()
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if(region === 'select a region'){
-      return
-    }else{
-      dispatch(fetchRegion(region))
-    }
+    dispatch(fetchCountry(country))
   }
 
   const handleChange = ({target: { value }}) => {
-    setRegion(value)
+    setCountry(value)
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <select onChange={handleChange}>
-          {regions.map(region => (
-            <option key={region} value={region}>{region}</option>
-          ))}
-        </select>
+        <input type="text" value={country} onChange={handleChange} />
         <input type="submit"/>
       </form>
     </div>
   )
 }
 
-export default FilterForm;
+export default SearchForm;
